ac321a32605329c12e101d3c385fcd8867665250
William Liu <will.liu1705@gmail.com>
2026-03-13 19:58:50 -0400

    Add delete button for tasks

--- src/tasksDOM.js
+++ src/tasksDOM.js
@@ -5,6 +5,9 @@ const tasksDiv = document.querySelector(".tasks");
 function addTask(task) {
   const singleTaskDiv = document.createElement("div");
   singleTaskDiv.classList.add("task-card");
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.classList.add("delete-button");
   const titleDiv = document.createElement("div");
   titleDiv.textContent = task.title;
   const descriptionP = document.createElement("p");
@@ -14,6 +17,7 @@ function addTask(task) {
   const priorityDiv = document.createElement("div");
   priorityDiv.textContent = task.priority;
 
+  singleTaskDiv.append(deleteButton);
   singleTaskDiv.append(titleDiv);
   singleTaskDiv.append(descriptionP);
   singleTaskDiv.append(dueDateDiv);
