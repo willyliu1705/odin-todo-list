58b17057aba05f06a306ce7f533481c0f138e0b9
William Liu <will.liu1705@gmail.com>
2026-03-13 19:39:34 -0400

    Include other task details in task card

--- src/tasksDOM.js
+++ src/tasksDOM.js
@@ -7,10 +7,17 @@ function addTask(task) {
   singleTaskDiv.classList.add("task-card");
   const titleDiv = document.createElement("div");
   titleDiv.textContent = task.title;
+  const descriptionP = document.createElement("p");
+  descriptionP.textContent = task.description;
   const dueDateDiv = document.createElement("div");
   dueDateDiv.textContent = task.dueDate;
+  const priorityDiv = document.createElement("div");
+  priorityDiv.textContent = task.priority;
 
   singleTaskDiv.append(titleDiv);
+  singleTaskDiv.append(descriptionP);
   singleTaskDiv.append(dueDateDiv);
+  singleTaskDiv.append(priorityDiv);
+
   tasksDiv.append(singleTaskDiv);
 }
